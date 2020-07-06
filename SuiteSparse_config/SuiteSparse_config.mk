3ad90aac71a3432b736b69718e339dbc4dfd52a2
Timothy Davis <davis@tamu.edu>
2020-07-06 18:44:50 +0200

    version 5.7.0

--- SuiteSparse_config/SuiteSparse_config.mk
+++ SuiteSparse_config/SuiteSparse_config.mk
@@ -7,7 +7,7 @@
 # and GraphBLAS.  The configuration settings for GraphBLAS are determined by
 # GraphBLAS/CMakeLists.txt
 
-SUITESPARSE_VERSION = 5.6.0
+SUITESPARSE_VERSION = 5.7.0
 
 #===============================================================================
 # Options you can change without editing this file:
@@ -174,7 +174,8 @@ SUITESPARSE_VERSION = 5.6.0
             BLAS = -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -liomp5 -lpthread -lm
             LAPACK =
         else
-            # use the OpenBLAS at http://www.openblas.net (CAN BE VERY SLOW)
+            # use the OpenBLAS at http://www.openblas.net (CAN BE VERY SLOW;
+            # CHOLMOD can be slowed down by a factor of 100x in extreme cases)
             BLAS = -lopenblas
         endif
     endif
